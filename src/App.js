d6ccd73686ba217632ddabddb7c29d6a4ec73169
Yiğit Hacıefendioğlu <yigith1@gmail.com>
2022-01-06 13:54:54 +0300

    app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [todos, setTodos] = useState(readTodos());
+  const [title, setTitle] = useState("");
+  saveTodos(todos);
+
+  const formSubmitted = function (event) {
+    event.preventDefault();
+    setTodos(sortTodos([{ title: title, isDone: false }, ...todos]))
+    setTitle("");
+  };
+
+  const updateDone = function (event, index) {
+    const newTodos = [...todos];
+    newTodos[index].isDone = event.target.checked;
+    setTodos(sortTodos(newTodos));
+  };
+
+  const deleteTodo = function (event, index) {
+    event.preventDefault();
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My Todos</h1>
+
+      <form onSubmit={formSubmitted}>
+        <input value={title} onChange={e => setTitle(e.target.value)}
+          type="text" placeholder="What are you going to do?" required />
+        <button className="btn-add">Add</button>
+      </form>
+      <ul>
+        {todos.map((v, i) => <li key={i} className={v.isDone ? "done" : "undone"}>
+          <input type="checkbox" checked={v.isDone} onChange={e => updateDone(e, i)} />
+          <span className="title">{v.title}</span>
+          <a className="delete" href="#" onClick={e => deleteTodo(e, i)}>
+            <i className="fas fa-times"></i>
+          </a>
+        </li>)}
+      </ul>
     </div>
   );
+}
+
+function readTodos() {
+  const data = localStorage["data"];
+  return data ? JSON.parse(data) : [];
+}
+
+function saveTodos(todos) {
+  localStorage["data"] = JSON.stringify(todos);
+}
+
+function sortTodos(todos) {
+  todos.sort((a, b) => a.isDone - b.isDone);
+  return todos;
 }
 
 export default App;
